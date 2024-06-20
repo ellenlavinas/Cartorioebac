4fd5490b695fbe029b575edc8c4df6f2d97bf96e
ellenlavinas <ellenlavinas@outlook.com>
2024-06-20 10:32:53 +0200

    Add files via upload

--- Cartorio.c
+++ Cartorio.c
@@ -138,34 +138,30 @@ int main()
        
 	   switch (opcao) //inicio da seleção  do menu
 	  
-	  {  
+	{  
 	 	  
-		   case 1:
-	   	  registro(); //chamada de funções
-          break;
+		 case 1:
+	   	 registro(); //chamada de funções
+         break;
         
-          
-		  case 2:
-          consulta();
-          break;
+		 case 2:
+         consulta();
+         break;
        
-          
-		  case 3:
-          deletar();
-          break;
+		 case 3:
+         deletar();
+         break;
        
+		 case 4:
+         printf("Obrigado por utilizar o sistema!\n");
+         return 0;
+         break;
           
-		  case 4:
-          printf("Obrigado por utilizar o sistema!\n");
-          return 0;
-          break;
-          
-          
-          default:
-	      printf("Essa opção não está disponivel\n");
-          system("pause");
-	      break;
-	  }  //fim da seleção
+         default:
+	     printf("Essa opção não está disponivel\n");
+         system("pause");
+	     break;
+	} //fim da seleção
 	       
 	        }
          }
